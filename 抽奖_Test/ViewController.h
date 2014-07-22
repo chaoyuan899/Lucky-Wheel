9b78f19329c763df6945843afc08ca84463b4059
chaoyuan899 <chaoyuan899@gmail.com>
2014-07-22 14:36:33 +0800

    origin code

--- 抽奖_Test/ViewController.h
+++ 抽奖_Test/ViewController.h
@@ -9,5 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *label1;
+@property (strong, nonatomic) IBOutlet UITextField *labelTextField;
+@property (strong, nonatomic) IBOutlet UIImageView *plateImageView;
+@property (strong, nonatomic) IBOutlet UIImageView *rotateStaticImageView;
 
 @end
